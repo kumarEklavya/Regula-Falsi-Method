1c14ad4305abd94e7ea0723e212e97755224c908
Eklavya Kumar <115495337+kumarEklavya@users.noreply.github.com>
2022-11-28 18:21:29 +0530

    Add files via upload

---
+++ regular falsi.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+#define f(x) x*x*x-4*x+1
+#define e 0.001
+int main()
+{
+    int i =0;
+    float  x0,x1,x2,f0,f1,f2;
+    printf("enter the value of x0 and x1");
+    scanf("%f%f",&x0,&x1);
+    do
+    {
+        f0=f(x0);
+        f1= f(x1);
+        f2=f(x2);
+        x2= (x0*f1)-(x1*f0)/(f1-f0);
+        if(f0*f2<0)
+        {
+            x1=x2;
+            f1=f2;
+        }
+        else
+        {
+            x0=x2;
+            f0=f2;
+        }
+        i++;
+
+        printf("root=%f\n",x2);
+        printf("value of function is %f",f2);
+
+    }
+    while(fabs(f2)>e);
+
+}
